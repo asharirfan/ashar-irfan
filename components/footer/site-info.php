e12e74d3fcd9189342e63357930b32737dfb06ed
Ashar Irfan <mrasharirfan@gmail.com>
2017-09-23 22:08:10 +0500

    Customized footer 💯

---
+++ components/footer/site-info.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The template for displaying footer information.
+ *
+ * @package Cover2
+ */
+
+?>
+
+<div class="site-info">
+	<p class="copyrights">
+		<?php esc_html_e( 'Copyright @ Ashar Irfan.', 'cover2' ); ?>
+		<?php printf( esc_html__( 'Proudly published with WordPress.', 'cover2' ) ); ?>
+	</p>
+	<?php if ( has_nav_menu( 'jetpack-social-menu' ) ) : ?>
+		<p><?php cover2_social_menu(); ?></p>
+	<?php endif; ?>
+</div><!-- .site-info -->
